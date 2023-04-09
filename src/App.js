74002e97173521d68e099edd975b8290757bda2c
Benjamin Stubina <benjamin.stubina@gmail.com>
2023-04-09 03:11:56 -0400

    Create footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Header from './components/Header/Header';
-import Button from './components/Button/Button';
 import Homepage from './pages/Homepage/Homepage';
 
 function App() {
   return (
     <>
-      <Header />
       <Homepage />
     </>
   );
