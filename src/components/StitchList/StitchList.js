4de44863598581d97fc0fc039f21dceec2e4233c
Benjamin Stubina <benjaminstubina@Benjamins-MacBook-Pro.local>
2023-04-10 00:25:56 -0400

    Create Stitch List component

---
+++ src/components/StitchList/StitchList.js
@@ -0,0 +1,37 @@
+import './StitchList.scss';
+import Stitch from '../Stitch/Stitch';
+
+function StitchList({ stitches, country }) {
+
+    if (!stitches) {
+        return (
+            <h1>
+                Fetching Stiches!
+            </h1>
+        )
+    }
+
+    function countryChecker() {
+        if (country === 'US') {
+            return 'US';
+        }
+        else {
+            return 'UK';
+        }
+    }
+
+    return (
+        <section className='stich-list'>
+            {stitches.map((stitch) => {
+                return (
+                    <Stitch 
+                    stitch={stitch}
+                    country={countryChecker(country)}
+                    />
+                )
+            })}
+        </section>
+    )
+}
+
+export default StitchList;
